09c6f90521d77a212fbc320cba944abca3521aae
memset0 <memset0@outlook.com>
2020-08-27 13:12:55 +0800

    用 pify 重写 dig bot 异步

--- src/bot/dig.js
+++ src/bot/dig.js
@@ -1,4 +1,5 @@
-const moment=require('moment');
+const pify = require('pify');
+const moment = require('moment');
 const mineflayer = require('mineflayer');
 const { Vec3 } = require('vec3');
 
@@ -11,12 +12,18 @@ function main(bot, targetX, targetY, targetZ) {
 	if (targetZ[0] > targetZ[1]) targetZ = (targetZ[1], targetZ[0]);
 
 
-	bot.on('chat', function (username, message) {
+	const log = (message) => {
+		// bot.chat(message);
+		console.log('<bot message>', message);
+	};
+	
+
+	bot.on('chat', (username, message) => {
 		const proc = {
-			hello: function () {
+			hello: () => {
 				bot.chat('hello');
 			},
-			status: function () {
+			status: () => {
 				bot.chat(`Health: ${bot.health} Food: ${bot.food} Time: ${bot.time.time} Digging: ${bot.targetDigBlock ? true : false}`);
 			}
 		};
@@ -31,63 +38,56 @@ function main(bot, targetX, targetY, targetZ) {
 	});
 
 
-	bot.once('spawn', function () {
-		const nextPosition = function (x, z) {
+	bot.once('spawn', () => {
+		const nextPosition = (x, z) => {
 			let way = (x - targetX[0]) % 2 ? -1 : 1;
-			if (way == 1 && z == targetZ[1]) {
-				if (x == targetX[1]) return null;
-				return [x + 1, z];
-			} else if (way == -1 && z == targetZ[0]) {
-				if (x == targetX[1]) return null;
-				return [x + 1, z];
+			if ((way == 1 && z == targetZ[1]) || (way == -1 && z == targetZ[0])) {
+				x += 1;
 			} else {
-				return [x, z + way];
+				z += way;
 			}
+			if (x > targetX[1]) return null;
+			return { x: x, z: z };
 		};
 
-		const solve = function (x, z) {
-			bot.chat(`/tp ${x} ${targetY[1]} ${z}`);
-			console.log('solve', x, z);
+		const blockBlackList = ['air', 'vine'];
 
-			let blockList = [];
-			for (let y = targetY[1]; y >= targetY[0]; y--) {
-				let vec3 = new Vec3(x, y, z);
-				let block = bot.blockAt(vec3);
-
-				if (!block || block.name == 'air') {
-					continue;
-				}
-
-				blockList.push(block);
-			}
-
-			function dig(id) {
-				if (id < blockList.length) {
-					let block = blockList[id];
-					bot.chat(`/tp ${x} ${block.position.y + 2} ${z}`);
-					if (block) {
-						console.log(moment().format('HH:mm:ss'), block.position.x, block.position.y, block.position.z, block.name);
-						bot.dig(block, () => dig(id + 1));
-					}
-				} else {
-					let next = nextPosition(x, z);
-					console.log('next', next);
-					if (next) {
-						solve(next[0], next[1]);
-					} else {
-						bot.chat('end!');
-					}
-				}
-			}
-			dig(0);
-		};
-
-		const main = function () {
+		const main = async () => {
 			console.log('Hello, World!');
 			console.log(targetX, targetY, targetZ);
 
-			// bot.chat('start!');
-			solve(targetX[0], targetZ[0]);
+			log('start!');
+
+			let x = targetX[0];
+			let z = targetZ[0];
+			while (true) {
+				bot.chat(`/tp ${x} ${targetY[1]} ${z}`);
+				console.log('solve', x, z);
+
+				for (let y = targetY[1]; y >= targetY[0]; y--) {
+					let vec3 = new Vec3(x, y, z);
+					let block = bot.blockAt(vec3);
+
+					if (!block || blockBlackList.includes(block.name)) {
+						continue;
+					}
+
+					bot.chat(`/tp ${x} ${block.position.y + 2} ${z}`);
+					console.log(moment().format('HH:mm:ss'), block.position.x, block.position.y, block.position.z, block.name);
+					await pify(bot.dig)(block);
+				}
+
+				let next = nextPosition(x, z);
+				console.log('next', next);
+				if (next) {
+					x = next.x;
+					z = next.z;
+				} else {
+					break;
+				}
+			}
+
+			log('end!');
 		};
 
 		setTimeout(main, 500);
@@ -104,11 +104,11 @@ if (require.main == module) {
 	});
 
 	bot.on('respawn', function () {
-		// bot.chat('spawn!');
+		log('spawn!');
 
-		// bot.stopDigging(); ?
+		// bot.stopDigging(); cause bug ?
 	});
-	
+
 	bot.once('spawn', function () {
 		bot.chat(`/login ${config.bot.password}`);
 	});
